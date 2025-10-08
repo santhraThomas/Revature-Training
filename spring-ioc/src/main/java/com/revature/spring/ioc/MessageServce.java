f1e21f6f7819e774530cc274eaea845e595ed481
Santhra Thomas <gn7030@wayne.edu>
2025-10-07 21:15:38 -0400

    Updated the application to use a setter method instead of constructor method.

--- spring-ioc/src/main/java/com/revature/spring/ioc/MessageServce.java
+++ spring-ioc/src/main/java/com/revature/spring/ioc/MessageServce.java
@@ -3,7 +3,12 @@ package com.revature.spring.ioc;
 public class MessageServce {
     private String message;
 
-    public MessageServce(String message) {
+    //For setter method injection you need a No-Arg constructor
+    public MessageServce() {
+    }
+    
+    //Setter method for dependency injection so the message is now injected via the setter method
+    public void setMessage(String message) {
         this.message = message;
     }
 
